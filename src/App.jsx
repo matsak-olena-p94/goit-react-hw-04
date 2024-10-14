c7a007db37b2a4b28d8fce6faaa6c9ed3ee43369
Olena Matsak <161434803+matsak-olena-p94@users.noreply.github.com>
2024-10-14 11:07:40 +0300

    add one

--- src/App.jsx
+++ src/App.jsx
@@ -1,57 +1,83 @@
-import { useState, useEffect } from "react"
-import SearchBox from "./components/SearchBox/SearchBox"
-import ContactList from "./components/ContactList/ContactList"
-import ContactForm from "./components/ContactForm/ContactForm"
+import SearchBar from "./components/SearchBar/SearchBar";
+import ImageGallery from "./components/ImageGallery/ImageGallery";
+import Loader from "./components/Loader/Loader";
+import ErrorMessage from "./components/ErrorMessage/ErrorMessage";
+import LoadMoreBtn from "./components/LoadMoreBtn/LoadMoreBtn";
+import { useEffect, useState } from 'react';
+import { fetchArticles } from './services/api';
+import ImageModal from './components/ImageModal/ImageModal';
 
 function App() {
-  
-  const [contacts, setContacts] = useState(() => {
-    const contactsFromLocalStorage = window.localStorage.getItem("contacts");
-    if (contactsFromLocalStorage !== null) {
-      return JSON.parse(contactsFromLocalStorage)
-    }
-    return (
-      [
-        {id: 'id-1', name: 'Rosie Simpson', number: '459-12-56'},
-        {id: 'id-2', name: 'Hermione Kline', number: '443-89-12'},
-        {id: 'id-3', name: 'Eden Clements', number: '645-17-79'},
-        {id: 'id-4', name: 'Annie Copeland', number: '227-91-26'},
-      ]
-    )
-  }
-  )
-
-  const [query, setQuery] = useState("")
-
-  const   visibleContacts = contacts.filter((contact) => {
-    return contact.name.toLowerCase().includes(query.toLowerCase())
-  })
-
-  const deleteContact = (contactId) => {
-    setContacts((prevContacts) => {
-      return prevContacts.filter((contact) => contact.id !== contactId);
-    }, [contacts])
-  }
-
-  const addContact = (newContact) => {
-    setContacts((prevContacts)=>{
-      return [...prevContacts, newContact]
-    })
-  }
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [isError, setIsError] = useState(false);
+  const [page, setPage] = useState(1);
+  const [query, setQuery] = useState('');
+  const [selectedImage, setSelectedImage] = useState(null);
+  const [isModalOpen, setIsModalOpen] = useState(false); 
 
   useEffect(() => {
-    window.localStorage.setItem("contacts", JSON.stringify(contacts))
-  })
+    const fetchImages = async () => {
+      if (query === '') return; 
+      try {
+        setIsError(false);
+        setIsLoading(true);
 
-    return (
-      <div>
-  <h1>Phonebook</h1>
-  <ContactForm adding={addContact}/>  
-  <SearchBox value={query} onSearch={setQuery}/>
-  <ContactList contacts={visibleContacts} onDelete={deleteContact}/>
-</div>
+        const response = await fetchArticles(query, page);
+        
+        if (page === 1) {
+          setImages(response.results);
+        } else {
+          setImages((prevImages) => [...prevImages, ...response.results]);
+        }
+      } catch (error) {
+        setIsError(true);
+      } finally {
+        setIsLoading(false);
+      }
+    };
 
-    )
+    fetchImages();
+  }, [query, page]); 
+
+  const handleSearch = (newQuery) => {
+    setQuery(newQuery);  
+    setPage(1);  
+  };
+
+  const handleLoadMore = () => {
+    setPage((prevPage) => prevPage + 1);  
+  };
+
+  const openModal = (imageUrl) => {
+    setSelectedImage(imageUrl);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+    setSelectedImage(null);
+  };
+
+  return (
+    <>
+      <SearchBar onSearch={handleSearch} />
+      {images.length > 0 && <ImageGallery images={images} onImageClick={openModal} />}
+      {isLoading && <Loader />}
+      {isError && <ErrorMessage />}
+      {images.length > 0 && !isLoading && !isError && (
+        <LoadMoreBtn handleClick={handleLoadMore} />
+      )}
+
+      {selectedImage && (
+        <ImageModal 
+          isOpen={isModalOpen} 
+          onClose={closeModal} 
+          imageUrl={selectedImage} 
+        />
+      )}
+    </>
+  );
 }
 
-export default App
+export default App;
